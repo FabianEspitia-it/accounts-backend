346c753113ae1356744ad36f7fb71e111f45ea82
FabianEspitia-it <fabian@tally.legal>
2025-06-25 19:20:09 -0500

    first commit

---
+++ src/codes/crud.py
@@ -0,0 +1,55 @@
+import time
+
+from src.utils.disney_methods import *
+
+from src.utils.netflix_methods import *
+
+
+def get_code_email_by_email(email: str) -> str:
+
+    time.sleep(10)
+
+    return call_get_disney_session_code(user_email=email)
+
+
+def get_temporal_access_code_by_email(email: str):
+
+    EMAIL_SUBJECT = "Tu código de acceso temporal de Netflix".replace(" ", "")
+
+    time.sleep(8)
+
+    return call_get_netflix_code_email(user_email=email, email_subject=EMAIL_SUBJECT)
+
+
+def get_home_code_by_email(email: str) -> str:
+
+    EMAIL_SUBJECT = "Importante:CómoactualizartuHogarconNetflix".replace(
+        " ", "")
+
+    time.sleep(8)
+    return call_get_netflix_code_email(user_email=email, email_subject=EMAIL_SUBJECT)
+
+
+def get_reset_password_by_email(email: str) -> str:
+
+    EMAIL_SUBJECT = "Completatusolicitudderestablecimientodecontraseña".replace(
+        " ", "")
+
+    time.sleep(8)
+
+    return call_get_netflix_code_email(user_email=email, email_subject=EMAIL_SUBJECT)
+
+
+def netflix_session_code_by_email(email: str) -> str:
+
+    time.sleep(8)
+
+    return call_get_netflix_session_code(user_email=email)
+
+def get_new_session_by_email(email: str) -> str:
+
+    EMAIL_SUBJECT = "Netflix:Nuevasolicituddeiniciodesesión".replace(" ", "") 
+
+    time.sleep(8)
+
+    return call_get_netflix_code_email(user_email=email, email_subject=EMAIL_SUBJECT)
---
+++ src/codes/router.py
@@ -0,0 +1,101 @@
+from fastapi import APIRouter, status, HTTPException
+from fastapi.responses import JSONResponse
+from src.codes.schemas import SessionCodes
+
+from src.codes.crud import *
+
+
+codes_router = APIRouter()
+
+
+@codes_router.post("/disney/session_code/", tags=["disney_codes"])
+def get_code_email(user_input: SessionCodes) -> JSONResponse:
+
+    if user_input.password not in (os.getenv("DISNEY_PASSWORDS_CODE").replace(" ", "").replace("[", "").replace("]", "")).split(","):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    try:
+        code = get_code_email_by_email(email=user_input.email)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    if not code:
+        raise HTTPException(status_code=404, detail="Code not found")
+
+    return JSONResponse(content={
+        "code": code,
+    }, status_code=status.HTTP_200_OK)
+
+
+@codes_router.get("/netflix/temporal_access/{email}", tags=["netflix_codes"])
+def get_temporal_access(email: str) -> JSONResponse:
+
+    try:
+
+        link = get_temporal_access_code_by_email(email=email)
+
+        if not link:
+            raise HTTPException(status_code=404, detail="Link not found")
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    return JSONResponse(content={"link": link}, status_code=status.HTTP_200_OK)
+
+
+@codes_router.get("/netflix/home_code/{email}", tags=["netflix_codes"])
+def get_home_code(email: str) -> JSONResponse:
+
+    try:
+        link = get_home_code_by_email(email=email)
+
+        if not link:
+            raise HTTPException(status_code=404, detail="Link not found")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    return JSONResponse(content={"link": link}, status_code=status.HTTP_200_OK)
+
+
+@codes_router.post("/netflix/session_code/", tags=["netflix_codes"])
+def get_session_code(user_input: SessionCodes) -> JSONResponse:
+
+    if user_input.password not in (os.getenv("NETFLIX_PASSWORDS_CODE").replace(" ", "").replace("[", "").replace("]", "")).split(","):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    code = netflix_session_code_by_email(email=user_input.email)
+
+    if not code:
+        raise HTTPException(status_code=404, detail="Code not found")
+
+    return JSONResponse(content={"code": code}, status_code=status.HTTP_200_OK)
+
+
+@codes_router.post("/netflix/password_reset/", tags=["netflix_codes"])
+def reset_password(user_input: SessionCodes) -> JSONResponse:
+
+    if user_input.password not in (os.getenv("NETFLIX_PASSWORDS_CODE").replace(" ", "").replace("[", "").replace("]", "")).split(","):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    try:
+        link = get_reset_password_by_email(email=user_input.email)
+
+        if not link:
+            raise HTTPException(status_code=404, detail="Link not found")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    return JSONResponse(content={"link": link}, status_code=status.HTTP_200_OK)
+
+
+@codes_router.get("/netflix/new_session/{email}", tags=["netflix_codes"])
+def new_session(email: str) -> JSONResponse:
+
+    try:
+        link = get_new_session_by_email(email=email)
+
+        if not link:
+            raise HTTPException(status_code=404, detail="Link not found")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    return JSONResponse(content={"link": link}, status_code=status.HTTP_200_OK)
